fc7e4fc22f64fe2623cf71d44d0ae492e0294046
shashi-123jha <ranjanshashi854105@gmail.com>
2024-04-21 22:30:43 +0530

    amazonee

--- script.js
+++ script.js
@@ -115,23 +115,6 @@ crsrb.style.top = dets.y - 250 +"px"
    
 })
 
-var h4all = document.querySelectorAll("#nav h4")
-h4all.forEach(function(elem){
-  elem.addEventListener("mouseenter",function(){
-    crsr.style.scale=3
-    crsr.style.border = "1px solid #fff"
-    crsr.style.backgroundColor = "transparent"
-
-  })
-
-  elem.addEventListener("mouseleave",function(){
-    crsr.style.scale=1
-    crsr.style.border = "0px solid #ace022"
-    crsr.style.backgroundColor = "#95C11E"
-    
-  })
-})
-
 
 
 document.addEventListener("mousemove" , function(dets){
